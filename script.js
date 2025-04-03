9f8a1f28f48ea6a21a22a511b4fa88c300ff240f
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-04-02 23:07:41 -0600

    closer

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-console.log("working");
-aboutButton.addEventListener("mouseenter", () => {
-    aboutButton.innerHTML = "about";
-    console.log("in");
-});
-aboutButton.addEventListener("mouseleave", () => {
-    aboutButton.innerHTML = "";
-});
+// console.log("working");
+// aboutButton.addEventListener("mouseenter", () => {
+//     aboutButton.innerHTML = "about";
+//     console.log("in");
+// });
+// aboutButton.addEventListener("mouseleave", () => {
+//     aboutButton.innerHTML = "";
+// });
 
-moreButton.addEventListener("mouseenter", () => {
-    moreButton.innerHTML = "learn more";
-});
-moreButton.addEventListener("mouseleave", () => {
-    moreButton.innerHTML = "";
-});
+// moreButton.addEventListener("mouseenter", () => {
+//     moreButton.innerHTML = "learn more";
+// });
+// moreButton.addEventListener("mouseleave", () => {
+//     moreButton.innerHTML = "";
+// });
